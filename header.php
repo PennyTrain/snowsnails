71c95b84ed7690b715612386bce9e6dae9899ec5
Train, Penny <Penny.Train@watlow.com>
2026-02-24 13:00:23 +0000

    Edit file name, and file paths

--- header.php
+++ header.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex, nofollow">
     <title>Snows Nails</title>
-    <link rel="icon" type="image/png" href="./assets/images/favicon/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/svg+xml" href="./assets/images/favicon/favicon.svg">
-    <link rel="shortcut icon" href="./assets/images/favicon/favicon.ico">
-    <link rel="apple-touch-icon" sizes="180x180" href="./assets/images/favicon/apple-touch-icon.png">
-    <link rel="manifest" href="./assets/images/favicon/site.webmanifest">
+    <link rel="icon" type="image/png" href="/assets/images/favicon/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/svg+xml" href="/assets/images/favicon/favicon.svg">
+    <link rel="shortcut icon" href="/assets/images/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicon/apple-touch-icon.png">
+    <link rel="manifest" href="/assets/images/favicon/site.webmanifest">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
 </head>
 
 <body>
@@ -24,7 +24,7 @@
         <!-- NAVBAR -->
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid moving-nav">
-                <a class="navbar-brand link logo" href="./index.php">
+                <a class="navbar-brand link logo" href="/index.php">
                     <span class="never-display">Logo that goes to home page</span>
                     <i class="fa-solid fa-b"></i>
                     <i class="fa-brands fa-d-and-d"></i>
@@ -37,21 +37,24 @@
                 <div class="collapse navbar-collapse" id="navbarsExample05">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active link" aria-current="page" href="./index.php">Home</a>
+                            <a class="nav-link active link" aria-current="page" href="/index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link link" href="./form.php">Contact Us</a>
+                            <a class="nav-link link" href="/bookingform.php">Contact Us</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle link" href="#" id="dropdown05" data-bs-toggle="dropdown"
                                 aria-expanded="false">What We Offer</a>
                             <ul class="dropdown-menu dropdown-container" aria-labelledby="dropdown05">
-                                <li><a class="dropdown-item link" href="./nails.php">Nails</a></li>
-                                <li><a class="dropdown-item link" href="./lashes.php">Lashes</a></li>
-                                <li><a class="dropdown-item link" href="./treatments.php">Body Treatments</a></li>
+                                <li><a class="dropdown-item link" href="/nails.php">Nails</a></li>
+                                <li><a class="dropdown-item link" href="/lashes.php">Lashes</a></li>
+                                <li><a class="dropdown-item link" href="/treatments.php">Body Treatments</a></li>
                             </ul>
                         </li>
                     </ul>
+                </div>
+                <div>
+                    <i class="fa-regular fa-circle-user login"></i>
                 </div>
             </div>
         </nav>
