947c7e63d9bf3f93097f43280e508b95987b34fd
Train, Penny <Penny.Train@watlow.com>
2026-05-13 13:31:02 +0100

    Getting code heroku ready

--- config/db.php
+++ config/db.php
@@ -1,24 +1,31 @@
 <?php
 
-$host = "localhost";
-$dbname = "snowsnails";
-$username = "root";
-$password = "";
+if (getenv('JAWSDB_URL')) {
+    // Running on Heroku (JawsDB MySQL)
+    $url = parse_url(getenv('JAWSDB_URL'));
+
+    $host = $url['host'];
+    $dbname = ltrim($url['path'], '/');
+    $username = $url['user'];
+    $password = $url['pass'];
+} else {
+    // Local development (your current setup)
+    $host = "localhost";
+    $dbname = "snowsnails";
+    $username = "root";
+    $password = "";
+}
 
 try {
     $conn = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8",
         $username,
-        $password,
+        $password
     );
 
-    // Set error mode
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Optional: fetch associative arrays by default
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
-
-//var_dump($conn) FOR DEBUGGING
